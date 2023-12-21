0971c2e1f9acdefae57e91cebfa61ac4ceaf58fb
Candela Ares <cande.ares15@gmail.com>
2023-12-21 18:05:51 -0300

    archivos

---
+++ routes/admin/login.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var router = express.Router();
+var usuariosModel = require('./../../models/usuariosModel');
+
+router.get('/', function (req, res, next) {
+    res.render('admin/login', {
+        layout:'admin/layout'
+    });
+  });
+
+  /*para destruir variables de session*/
+  router.get('/logout', function (req, res, next) {
+    req.session.destroy(); //destruir
+    res.render('admin/login', {
+        layout: 'admin/layout'
+    })
+  })
+
+  router.post('/', async (req, res, next) => {
+    try{
+        console.log(req.body);
+        var usuario = req.body.usuario;
+        var password = req.body.password;
+
+        console.log(req.body);
+
+        var data = await usuariosModel.getUserAndPassword (usuario, password);
+
+        if (data != undefined) {
+            req.session.id_usuario = data.id;  //id<nombre de la columna
+            req.session.nombre = data.usuario;
+            //req.session.password = data.password;
+            res.redirect('/admin/novedades');
+        } else {
+            res.render('admin/login', {
+                layout: 'admin/layout',
+                error: true
+            })
+        } //cierre else
+    } catch (error) {
+        console.log(error)
+    }
+  })
+
+  module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,36 @@
 var express = require('express');
 var router = express.Router();
 var nodemailer = require('nodemailer');
+var novedadesModel = require('../models/novedadesModel');
+var cloudinary = require('cloudinary').v2;
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async function (req, res, next) {
+
+  var novedades = await novedadesModel.getNovedades();
+
+  novedades = novedades.splice(0, 5);
+  novedades = novedades.map(novedad => {
+    if (novedad.img_id) {
+      const imagen = cloudinary.url(novedad.img_id, {
+        width: 460,
+        crop: 'fill'
+      });
+      return {
+        ...novedad,
+        imagen
+      }
+    } else {
+      return {
+        ...novedad,
+        imagen: '/images/noimage.jpg'
+      }
+    }
+  });
+
+  res.render('index', {
+    novedades
+  });
 });
 
 router.post('/', async (req, res, next) => {
@@ -20,7 +46,7 @@ router.post('/', async (req, res, next) => {
   var obj = {
     to: 'cande.ares15@gmail.com',
     subject: 'CONTACTO WEB',
-    html: nombre + " " + apellido + " se contacto a través de la web y quiere más información a este correo : " + mensaje + ". <br> Su tel es: " + tel
+    html: nombre + " " + apellido + " se contacto a través de la web y quiere más información a este correo : " + email + ". <br> Su tel es: " + tel + " . Dejó el siguiente mensaje : " + mensaje
   }
 
   var trasnporter = nodemailer.createTransport({
